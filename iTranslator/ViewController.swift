fd79d6e9b2d12b82ed110580e1cbdd7e81d8804d
baielvegas <30592102+baielvegas@users.noreply.github.com>
2018-12-04 16:31:14 +0600

    Initial Commit

---
+++ iTranslator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTranslator
+//
+//  Created by Isaev Baiel on 12/4/18.
+//  Copyright © 2018 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
